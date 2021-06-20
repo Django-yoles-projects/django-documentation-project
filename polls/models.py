ecf48e1d130532a62f32e1b1ed8d5302c5fbad22
yoles <yo-0ne@hotmail.fr>
2021-06-20 17:52:04 +0200

    Database API

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+from django.utils import timezone
+import datetime
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    published_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.published_date >= timezone.now() - datetime.timedelta(days=2)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
